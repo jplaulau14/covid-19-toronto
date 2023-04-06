27a24783c6f6b338c2bea210f376224b084639f0
jplaulau14 <prod.patricklaurel@gmail.com>
2023-04-06 14:28:33 +0800

    finished pipeline

---
+++ flows/main.py
@@ -0,0 +1,143 @@
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from prefect_gcp import GcpCredentials
+from io import StringIO
+import requests
+from pathlib import Path
+from datetime import datetime
+from prefect_dbt.cli.commands import DbtCoreOperation
+
+current_datetime = datetime.now()
+
+@task(retries=3, retry_delay_seconds=10, log_prints=True, name="Extract from Opendata Toronto")
+def extract() -> pd.DataFrame:
+    """
+    Extracts the date from Opendata Toronto's COVID-19 dataset
+    """
+    base_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca"
+
+    url = base_url + "/api/3/action/package_show"
+    params = {"id": "covid-19-cases-in-toronto"}
+    package = requests.get(url, params=params).json()
+
+    # To get resource data:
+    for idx, resource in enumerate(package["result"]["resources"]):
+
+        # for datastore_active resources:
+        if resource["datastore_active"]:
+
+            # To get all records in CSV format:
+            url = base_url + "/datastore/dump/" + resource["id"]
+            resource_dump_data = requests.get(url).text
+            df = pd.read_csv(StringIO(resource_dump_data))
+
+    return df
+
+
+@task(retries=3, retry_delay_seconds=10, log_prints=True, name="Convert to Parquet")
+def convert_to_parquet(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Converts the dataframe to parquet format
+    """
+    df.to_parquet("data/covid19-toronto.parquet")
+    df = pd.read_parquet("data/covid19-toronto.parquet")
+    return df
+
+
+@task(retries=3, retry_delay_seconds=10, log_prints=True, name="Transform Data")
+def transform(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Transforms the dataframe
+    Converts Episode Date and Reported Date to datetime
+    """
+    df["Episode Date"] = pd.to_datetime(df["Episode Date"])
+    df["Reported Date"] = pd.to_datetime(df["Reported Date"])
+    return df
+
+
+@task(retries=3, retry_delay_seconds=10, log_prints=True, name="Load to Local Disk")
+def load_local(df: pd.DataFrame) -> Path:
+    """
+    Loads the dataframe to local disk
+    """
+    df.to_parquet(f"data/{current_datetime}-covid19-toronto.parquet")
+    return Path(f"data/{current_datetime}-covid19-toronto.parquet")
+
+
+@task(retries=3, retry_delay_seconds=10, log_prints=True)
+def load_to_gcs(path: Path):
+    """
+    Loads the dataframe to GCS
+    """
+    gcs_block = GcsBucket.load('gcs-block')
+    gcs_block.upload_from_path(from_path=path, to_path=path)
+    return
+
+@task(retries=3, retry_delay_seconds=10, log_prints=True, name="Extract from GCS")
+def extract_from_gcs() -> Path:
+    """
+    Extracts the date from GCS
+    """
+    gcs_path = f"data/{current_datetime}-covid19-toronto.parquet"
+    gcs_block = GcsBucket.load('gcs-block')
+    gcs_block.download_object_to_path(
+        from_path=gcs_path,
+        to_path=gcs_path
+    )
+
+    return Path(gcs_path)
+
+@task(retries=3, retry_delay_seconds=10, log_prints=True, name="Rename column headers")
+def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Transforms the dataframe
+    Converts Episode Date and Reported Date to datetime
+    """
+    df.columns = df.columns.str.lower().str.replace(" ", "_")
+    return df
+
+@task(retries=3, retry_delay_seconds=10, log_prints=True, name="Load to BigQuery")
+def load_to_bq(df: pd.DataFrame):
+    """
+    Loads the dataframe to BigQuery
+    """
+    gcp_credentials_block = GcpCredentials.load("c19-gcp-creds")
+    df.to_gbq(destination_table='covid19_toronto.covid_data', if_exists="replace", project_id="covid-19-toronto",
+              credentials=gcp_credentials_block.get_credentials_from_service_account())
+    return
+
+@task(retries=3, retry_delay_seconds=10, log_prints=True, name="Run dbt")
+def run_dbt():
+    """
+    Run dbt models
+    """
+    dbt_path = "project_dbt"
+
+    dbt_run = DbtCoreOperation(
+        commands=["dbt run"],
+        project_dir=dbt_path,
+        profiles_dir=dbt_path,
+    )
+
+    dbt_run.run()
+
+    return
+
+@flow(name="ETL Flow")
+def main_flow():
+    df = extract()
+    df = transform(df)
+    df = rename_columns(df)
+    df = convert_to_parquet(df)
+    path = load_local(df)
+    load_to_gcs(path)
+    path = extract_from_gcs()
+    df = pd.read_parquet(path)
+    df = rename_columns(df)
+    print(df.head())
+    load_to_bq(df)
+    run_dbt()
+
+if __name__ == "__main__":
+    main_flow()
